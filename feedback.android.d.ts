51547c7a677a04233de55502ccf8379e1300cd52
EddyVerbruggen <eddyverbruggen@gmail.com>
2017-04-20 12:08:20 +0200

    cleanup

--- feedback.android.d.ts
+++
@@ -1,9 +0,0 @@
-import { FeedbackCommon, FeedbackShowOptions, FeedbackHideOptions } from "./feedback.common";
-export declare class Feedback extends FeedbackCommon {
-    private lastAlert;
-    show(options: FeedbackShowOptions): Promise<any>;
-    private static getBackgroundColor(type?);
-    private static getIconResourceId(resourcename);
-    private static getIconName(type?);
-    hide(options?: FeedbackHideOptions): Promise<any>;
-}
--- feedback.ios.d.ts
+++
@@ -1,7 +0,0 @@
-import { FeedbackCommon, FeedbackShowOptions, FeedbackHideOptions } from "./feedback.common";
-export declare class Feedback extends FeedbackCommon {
-    show(options: FeedbackShowOptions): Promise<any>;
-    hide(options?: FeedbackHideOptions): Promise<any>;
-    private static getType(type?);
-    private static getPosition(position?);
-}
