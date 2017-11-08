f198f492a9c9a1295a5da7a21b244dd3254d4c44
EddyVerbruggen <eddyverbruggen@gmail.com>
2017-11-08 16:27:14 +0100

    lint error

--- src/platforms/ios/ISMessages.d.ts
+++ src/platforms/ios/ISMessages.d.ts
@@ -1,47 +1,46 @@
-
 declare const enum ISAlertPosition {
 
-	Top = 0,
+  Top = 0,
 
-	Bottom = 1
+  Bottom = 1
 }
 
 declare const enum ISAlertType {
 
-	Success = 0,
+  Success = 0,
 
-	Error = 1,
+  Error = 1,
 
-	Warning = 2,
+  Warning = 2,
 
-	Info = 3,
+  Info = 3,
 
-	Custom = 4
+  Custom = 4
 }
 
 declare class ISMessages extends UIViewController {
 
-	static alloc(): ISMessages; // inherited from NSObject
+  static alloc(): ISMessages; // inherited from NSObject
 
-	static cardAlertWithTitleMessageIconImageDurationHideOnSwipeHideOnTapAlertTypeAlertPosition(title: string, message: string, iconImage: UIImage, duration: number, hideOnSwipe: boolean, hideOnTap: boolean, type: ISAlertType, position: ISAlertPosition): ISMessages;
+  static cardAlertWithTitleMessageIconImageDurationHideOnSwipeHideOnTapAlertTypeAlertPosition(title: string, message: string, iconImage: UIImage, duration: number, hideOnSwipe: boolean, hideOnTap: boolean, type: ISAlertType, position: ISAlertPosition): ISMessages;
 
-	static hideAlertAnimated(animated: boolean): void;
+  static hideAlertAnimated(animated: boolean): void;
 
-	static new(): ISMessages; // inherited from NSObject
+  static new(): ISMessages; // inherited from NSObject
 
-	static showCardAlertWithTitleMessageDurationHideOnSwipeHideOnTapAlertTypeAlertPositionDidHide(title: string, message: string, duration: number, hideOnSwipe: boolean, hideOnTap: boolean, type: ISAlertType, position: ISAlertPosition, didHide: (p1: boolean) => void): ISMessages;
+  static showCardAlertWithTitleMessageDurationHideOnSwipeHideOnTapAlertTypeAlertPositionDidHide(title: string, message: string, duration: number, hideOnSwipe: boolean, hideOnTap: boolean, type: ISAlertType, position: ISAlertPosition, didHide: (p1: boolean) => void): ISMessages;
 
-	alertViewBackgroundColor: UIColor;
+  alertViewBackgroundColor: UIColor;
 
-	messageLabelFont: UIFont;
+  messageLabelFont: UIFont;
 
-	messageLabelTextColor: UIColor;
+  messageLabelTextColor: UIColor;
 
-	titleLabelFont: UIFont;
+  titleLabelFont: UIFont;
 
-	titleLabelTextColor: UIColor;
+  titleLabelTextColor: UIColor;
 
-	showDidHide(handler: () => void, didHide: (p1: boolean) => void): void;
+  showDidHide(handler: () => void, didHide: (p1: boolean) => void): void;
 }
 
 declare var ISMessagesVersionNumber: number;
