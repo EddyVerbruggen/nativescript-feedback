95505cd3ecf5b890e95e2b6ac8ee7a96bff59ee0
EddyVerbruggen <eddyverbruggen@gmail.com>
2017-11-08 16:56:05 +0100

    cleanup

--- demo/references.d.ts
+++ demo/references.d.ts
@@ -1,4 +1,2 @@
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
-/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
-
-/// <reference path="./node_modules/nativescript-feedback/platforms/ios/ISMessages.d.ts" />
+/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />s
